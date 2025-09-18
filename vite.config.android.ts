98908bca1fa3c851f597bc3a9b4d224ec3b4d219
verdu89 <verdu89@gmail.com>
2025-09-18 12:50:30 +0200

    diviso vite config

--- vite.config.android.ts
+++ vite.config.android.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/Ns-cantieri/",
+  base: "./",
   appType: "spa",
   plugins: [react()],
   resolve: {
