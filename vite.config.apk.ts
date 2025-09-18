49ff6ec94e29daced9d35dcac7b23be1ca77d3af
verdu89 <verdu89@gmail.com>
2025-09-18 13:48:44 +0200

    diviso file per la build

--- vite.config.apk.ts
+++ vite.config.apk.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "./",
+  base: "./", // ✅ Path relativo per APK
   appType: "spa",
   plugins: [react()],
   resolve: {
